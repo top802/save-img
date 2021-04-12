0a94ed9d627f413aaeb5132d93c86b08d3024d49
Alexander <tatrun89@gmail.com>
2021-04-13 00:29:24 +0300

    generat spring project

---
+++ img/src/test/java/top/example/img/SaveApplicationTests.java
@@ -0,0 +1,13 @@
+package top.example.img;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
