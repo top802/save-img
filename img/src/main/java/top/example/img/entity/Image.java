21936643aa7a36715a3c13cf1f1727f8a414dedb
Alexander <tatrun89@gmail.com>
2021-04-13 21:52:44 +0300

    added entities

--- img/src/main/java/top/example/img/entity/Image.java
+++ img/src/main/java/top/example/img/entity/Image.java
@@ -1,4 +1,25 @@
 package top.example.img.entity;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="image")
 public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "description")
+    private String firstName;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+
+
 }
--- img/src/main/java/top/example/img/entity/User.java
+++ img/src/main/java/top/example/img/entity/User.java
@@ -1,7 +1,27 @@
 package top.example.img.entity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.util.Date;
 
 @Entity
+@Table(name="user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Email(message = "Email should be valid")
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
 }
