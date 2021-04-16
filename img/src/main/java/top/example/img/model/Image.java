c388c1f828717aa7d22e5ac074ebf893949e2cae
Alexander <tatrun89@gmail.com>
2021-04-16 14:08:46 +0300

    wip

--- img/src/main/java/top/example/img/model/Image.java
+++ img/src/main/java/top/example/img/model/Image.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class Image {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(name = "image_url")
--- img/src/main/java/top/example/img/model/User.java
+++ img/src/main/java/top/example/img/model/User.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(name = "name")
