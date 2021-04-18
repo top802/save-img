a4c4d0d78bd5612d70aa9634d8efa4d1d430c047
Alexander <tatrun89@gmail.com>
2021-04-18 14:44:22 +0300

    wip added web security

--- img/src/main/java/top/example/img/auth/web/UserController.java
+++ img/src/main/java/top/example/img/auth/web/UserController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import top.example.img.auth.repository.UserRepository;
 import top.example.img.auth.service.UserService;
 
 @Controller
+@RequestMapping("/pages")
 public class UserController {
 
     @Autowired
@@ -18,21 +20,21 @@ public class UserController {
     private UserRepository userRepository;
 
 
-    @GetMapping("/pages//login")
+    @GetMapping(value = "/login")
     public String login(Model model, String error){
         if (error != null)
             model.addAttribute("error", "Your email and password is invalid");
         return "login";
     }
 
-    @GetMapping("/pages/signup")
+    @GetMapping(value = "/signup")
     public String signUp(Model model, String error){
         if (error != null)
             model.addAttribute("error", "Your email and password is invalid");
         return "signUp";
     }
 
-    @GetMapping("/pages/forgot-password")
+    @GetMapping(value = "/forgot-password")
     public String forgotPassword(@RequestParam(name="name", required=false, defaultValue="PASSWORD") String name, Model model){
         model.addAttribute("name", name);
         return "forgotPassword";
